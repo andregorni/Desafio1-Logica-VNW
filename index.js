a75babdd95e88a027338fcdfe10cc388ed9e26e1
andregorni <ac.gorni@gmail.com>
2021-05-11 21:44:18 -0300

    Desafio 1 - Logica - VNW

---
+++ index.js
@@ -0,0 +1,14 @@
+let nome ="André Cesar"
+let idade = 32
+let ano = 2021
+let nascimento = "Meu ano de nascimento é:"
+
+console.log(nascimento, ano - idade)
+
+
+
+let souhumano = true
+let primary = 14
+let secondary = 27
+
+console.log(primary + secondary)
